201cc171c3e4a9ac114d0b736f234b518770add9
Chet Gray <chetgray@gmail.com>
2015-08-24 00:03:37 -0400

    Use an Enum for dance role (gent/lady)
    This implementation could change later. As it is, the internal value for
    gent is -1 and the internal value for lady is 1. This simplifies
    "negation" of the role.
    
    The roles are also made available as module-level names: GENT and LADY.
    This should ease possible future changes of implementation.

--- partnering.py
+++ partnering.py
@@ -1,7 +1,27 @@
 """Agent-based model simulation of contra dance partner selection."""
 
 
+import enum
+
 import mesa
+
+
+class Role(enum.Enum):
+    gent = -1
+    lady = 1
+
+    def __neg__(self):
+        """Return the opposite role.
+
+        >>> -Role.gent is Role.lady
+        True
+        >>> -Role.lady is Role.gent
+        True
+        """
+        return Role(-self.value)
+
+GENT = Role.gent
+LADY = Role.lady
 
 
 class Dancer(mesa.Agent):
